85f1335ce80d0a68044130804b7df3e55fe54415
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2020-10-11 15:09:03 +0700

    Add sample Admin LTE Template

--- LivelCmsSystem/LivelCmsSystem/Controllers/HomeController.cs
+++ LivelCmsSystem/LivelCmsSystem/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace LivelCmsSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
